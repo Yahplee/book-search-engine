470da4da1fb89d82ac09ee8bd1c03e07ceab7b9e
Andrew Lee <alee68@fordham.edu>
2022-09-21 22:26:49 -0400

    added body token in auth.js, created typeDefs and referenced apollo in server.js of the server side

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require("apollo-server-express");
+const typeDefs = gql`
+	type User {
+		_id: ID
+		username: String
+		email: String
+		bookCount: String
+		savedBooks: [Book]
+	}
+	type Book {
+		_id: ID
+		authors: [String]
+		description: String
+		bookId: String
+		image: String
+		link: String
+		title: String
+	}
+	type Auth {
+		token: ID
+		user: User
+	}
+	type Query {
+		me: User
+		users: [User]
+		user(username: String!): User
+		books(username: String): [Book]
+		book(_id: ID!): Book
+	}
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String): Auth
+		saveBook(
+			authors: [String!]
+			description: String!
+			title: String!
+			bookId: String!
+			image: String!
+			link: String!
+		): User
+		removeBook(bookId: String!): User
+	}
+`;
+
+module.exports = typeDefs;
